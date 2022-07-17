c672c61c751e51affd01b35a8224f6d36aeeda19
devanthai <thailenro123@gmail.com>
2022-07-17 09:51:23 +0700

    ahihi

--- auto/momoAuto.js
+++ auto/momoAuto.js
@@ -20,7 +20,10 @@ setInterval(async () => {
                         const description = element.noidung.toLowerCase()
                         var creditAmount = element.sotien
                         creditAmount = Number(creditAmount)
+
                         if (creditAmount != 0 && description.split("naptien").length == 2) {
+                            console.log(description,creditAmount)
+
                             const deposits = await Deposit.find({ $text: { $search: description }, status: 0 })
                             var donpick = null
                             deposits.forEach(elementz => {
@@ -28,6 +31,8 @@ setInterval(async () => {
                                     donpick = elementz
                                 }
                             });
+                            console.log(donpick)
+
                             if (donpick != null && donpick.gate.toLowerCase().includes("mm")) {
                                 if (creditAmount == donpick.amount) {
                                     await Deposit.findByIdAndUpdate(donpick._id, { status: 1 })
